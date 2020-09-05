7e58af03d84b978c607212c3b3d023e7aeb42cf6
MollyZalman <68440011+MollyZalman@users.noreply.github.com>
2020-09-05 14:28:41 -0400

    Hopefully Fixing File Issue

---
+++ js/script.js
@@ -0,0 +1,46 @@
+//Autofocus feature for first text field
+document.getElementById('name').focus();
+
+
+
+
+
+
+// function isValidName (name){
+
+// }
+
+// function isValidEmail (email){
+
+// }
+
+// function isValidCardNumber (card-number){
+
+// }
+
+// function isValidZipCode (zip-code){
+
+// }
+
+// function isValidCvv (cvv){
+
+// }
+
+
+
+// const nameInput = document.getElementById("name");
+// const emailInput = document.getElementById("email");
+// const cardNumberInput = document.getElementById("card-number");
+// const zipCodeInput = document.getElementById("zip-code");
+// const cvvInput = document.getElementById("cvv");
+
+// nameInput.addEventListener("input", createListener(isValidName));
+
+// emailInput.addEventListener("input", createListener(isValidEmail));
+
+// cardNumberInput.addEventListener("input", createListener(isValidCardNumber));
+
+// zipCodeInput.addEventListener("input", createListener(isValidZipCode));
+
+// cvvInput.addEventListener("input", createListener(isValidCvv));
+
